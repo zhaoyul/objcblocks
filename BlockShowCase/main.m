443dafa7928e485b5f17e153ab44592561f12109
Kevin.Li <li_zhaoyu@msn.com>
2015-06-03 23:12:43 +0800

    Initial Commit

---
+++ BlockShowCase/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BlockShowCase
+//
+//  Created by Zhaoyu Li on 3/6/15.
+//  Copyright (c) 2015 Zhaoyu Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
